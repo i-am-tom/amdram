92904d862bb1038fd6350b89d849710796869c21
Tom Harding <tomjharding@live.co.uk>
2023-02-23 13:59:40 +0000

    Add overloaded vector constructors
    GLSL allows some weird and wonderful ways to produce vectors. I've tried
    to replicate the full set of overloaded alternatives with some class
    incoherence magic.

--- shaders/source/Shader/Expression.hs
+++ shaders/source/Shader/Expression.hs
@@ -1,11 +1,22 @@
+{-# LANGUAGE NoImplicitPrelude #-}
+
 -- |
 -- Typed syntactic sugar on top of @language-glsl@.
 module Shader.Expression
   ( Expr (toGLSL),
+
+    -- * GLSL Embedding
+    fromInteger,
+    fromRational,
     lift,
+
+    -- * Vector Constructors
+    vec2,
+    vec3,
     vec4,
   )
 where
 
-import Shader.Expression.Constants (lift)
-import Shader.Expression.Core (Expr (toGLSL), vec4)
+import Shader.Expression.Constants (fromInteger, fromRational, lift)
+import Shader.Expression.Core (Expr (toGLSL))
+import Shader.Expression.Vector (vec2, vec3, vec4)
