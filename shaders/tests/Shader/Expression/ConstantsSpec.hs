5b55542756789f447de02b6f625e50527fb5812f
Tom Harding <tomjharding@live.co.uk>
2023-02-25 18:22:33 +0000

    Lift more vectors!
    Now we can start converting between types, it's useful to be able to
    lift other sizes and types of vector.

--- shaders/tests/Shader/Expression/ConstantsSpec.hs
+++ shaders/tests/Shader/Expression/ConstantsSpec.hs
@@ -1,17 +1,17 @@
 {-# LANGUAGE BlockArguments #-}
+{-# LANGUAGE OverloadedRecordDot #-}
 
 module Shader.Expression.ConstantsSpec where
 
 import Control.Monad.IO.Class (liftIO)
-import Graphics.Rendering.OpenGL (GLfloat)
+import Graphics.Rendering.OpenGL (GLfloat, GLint)
 import Hedgehog (Gen, forAll)
 import Hedgehog.Gen qualified as Gen
 import Hedgehog.Range qualified as Range
 import Helper.Renderer (Renderer, renderExpr)
 import Helper.Roughly (isRoughly)
-import Linear (V4 (V4))
-import Shader.Expression.Constants (lift)
-import Shader.Expression.Vector (vec4)
+import Linear (V2 (V2), V3 (V3), V4 (V4))
+import Shader.Expression (Expr, cast, ivec4, lift, vec4)
 import Test.Hspec (SpecWith, it)
 import Test.Hspec.Hedgehog (hedgehog)
 import Prelude hiding (fromInteger, fromRational)
@@ -22,17 +22,40 @@ genZeroToOne = Gen.float (Range.linearFrac 0 1)
 spec :: SpecWith Renderer
 spec = do
   it "lift @GLfloat" \renderer -> hedgehog do
-    input@(V4 x y z w) <- forAll do
-      x <- genZeroToOne
-      y <- genZeroToOne
-      z <- genZeroToOne
-
-      pure $ V4 @GLfloat x y z 1
+    x <- forAll genZeroToOne
+    y <- forAll genZeroToOne
+    z <- forAll genZeroToOne
 
     output <- liftIO $ renderExpr renderer do
-      vec4 (lift x) (lift y) (lift z) (lift w)
+      vec4 (lift x) (lift y) (lift z) (lift 1)
 
-    input `isRoughly` output
+    V4 x y z 1 `isRoughly` output
+
+  it "lift @(V2 GLfloat)" \renderer -> hedgehog do
+    x <- forAll genZeroToOne
+    y <- forAll genZeroToOne
+    z <- forAll genZeroToOne
+
+    output <- liftIO $ renderExpr renderer do
+      let partial :: Expr (V2 GLfloat)
+          partial = lift (V2 x y)
+
+      vec4 partial.x partial.y (lift z) (lift 1)
+
+    V4 x y z 1 `isRoughly` output
+
+  it "lift @(V3 GLfloat)" \renderer -> hedgehog do
+    x <- forAll genZeroToOne
+    y <- forAll genZeroToOne
+    z <- forAll genZeroToOne
+
+    output <- liftIO $ renderExpr renderer do
+      let partial :: Expr (V3 GLfloat)
+          partial = lift (V3 x y z)
+
+      vec4 partial.x partial.y partial.z (lift 1)
+
+    V4 x y z 1 `isRoughly` output
 
   it "lift @(V4 GLfloat)" \renderer -> hedgehog do
     input <- forAll do
@@ -44,3 +67,49 @@ spec = do
 
     output <- liftIO $ renderExpr renderer (lift input)
     input `isRoughly` output
+
+  it "lift @GLint" \renderer -> hedgehog do
+    x <- forAll $ Gen.element [0, 1]
+    y <- forAll $ Gen.element [0, 1]
+    z <- forAll $ Gen.element [0, 1]
+
+    output <- liftIO $ renderExpr renderer do
+      cast (ivec4 (lift x) (lift y) (lift z) (lift 1))
+
+    fmap fromIntegral (V4 x y z 1) `isRoughly` output
+
+  it "lift @(V2 GLint)" \renderer -> hedgehog do
+    x <- forAll $ Gen.element [0, 1]
+    y <- forAll $ Gen.element [0, 1]
+    z <- forAll $ Gen.element [0, 1]
+
+    output <- liftIO $ renderExpr renderer do
+      let partial :: Expr (V2 GLint)
+          partial = lift (V2 x y)
+
+      cast (ivec4 partial.x partial.y (lift z) (lift 1))
+
+    fmap fromIntegral (V4 x y z 1) `isRoughly` output
+
+  it "lift @(V3 GLint)" \renderer -> hedgehog do
+    x <- forAll $ Gen.element [0, 1]
+    y <- forAll $ Gen.element [0, 1]
+    z <- forAll $ Gen.element [0, 1]
+
+    output <- liftIO $ renderExpr renderer do
+      let partial :: Expr (V3 GLint)
+          partial = lift (V3 x y z)
+
+      cast (ivec4 partial.x partial.y partial.z (lift 1))
+
+    fmap fromIntegral (V4 x y z 1) `isRoughly` output
+
+  it "lift @(V4 GLint)" \renderer -> hedgehog do
+    x <- forAll $ Gen.element [0, 1]
+    y <- forAll $ Gen.element [0, 1]
+    z <- forAll $ Gen.element [0, 1]
+
+    output <- liftIO $ renderExpr renderer do
+      cast @(V4 GLint) (lift (V4 x y z 1))
+
+    fmap fromIntegral (V4 x y z 1) `isRoughly` output
