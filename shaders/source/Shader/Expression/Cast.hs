e5caf01f8158fee7dacc339fc489b7a702c08921
Tom Harding <tomjharding@live.co.uk>
2023-02-25 10:47:46 +0000

    Add implicit conversion support
    GLSL allows types to be converted to other types implicitly. Jury's out
    on whether this is a good thing. In any case, we now support implicit
    conversions in the form of _explicit_ conversions!
    
    This also gives us our first way to talk about non-`float` types as we
    can convert `int` and `uint` to `float` implicitly. This means our type
    system has expanded from `float` and `vec4` to `float`, `vec4`, `int`,
    _and_ `ivec4`! For the sake of testing, I've added an `ivec4`
    constructor.

---
+++ shaders/source/Shader/Expression/Cast.hs
@@ -0,0 +1,25 @@
+-- |
+-- A function for an implicit GLSL conversion.
+module Shader.Expression.Cast where
+
+import Data.Coerce (coerce)
+import Data.Kind (Constraint, Type)
+import Graphics.Rendering.OpenGL (GLfloat, GLint)
+import Linear (V4)
+import Shader.Expression.Core (Expr (Expr))
+
+-- | GLSL supports implicit conversions: if a @uint@ is expected and you give
+-- an @int@, then the @int@ can be implicitly converted into a @uint@. Because
+-- we /want/ types, we have to make our implicit conversions... well, explicit.
+-- This should be zero cost: in theory, @cast x@ should have the same GLSL
+-- representation as @x@.
+cast :: (Cast x y) => Expr x -> Expr y
+cast = coerce
+
+-- | Types that can be implicitly converted into other types.
+type Cast :: Type -> Type -> Constraint
+class Cast x y
+
+instance Cast GLint GLfloat
+
+instance (Cast x y) => Cast (V4 x) (V4 y)
