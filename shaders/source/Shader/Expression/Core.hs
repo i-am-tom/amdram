8ef0141f31a5b86e802afa1d5893a7c3517af410
Tom Harding <tomjharding@live.co.uk>
2023-02-28 09:43:43 +0000

    Revert to Cofree
    I think it's going to make the intermediate computation step /much/
    easier if we're always talking about the same type.
    
    We return to `Cofree TypeSpecifier ExprF` as our `Expr` representation.
    When we implement observable sharing, this means we get the DeRef
    structure, `CofreeF TypeSpecifier ExprF` for free. It also means that we
    can use recursion schemes to generate the AST in potentially more
    exciting ways (can we use a histo to avoid redundant intermediate
    values?)

--- shaders/source/Shader/Expression/Core.hs
+++ shaders/source/Shader/Expression/Core.hs
@@ -1,83 +1,81 @@
-{-# LANGUAGE BlockArguments #-}
 {-# LANGUAGE DataKinds #-}
 {-# LANGUAGE DerivingStrategies #-}
-{-# LANGUAGE LambdaCase #-}
-{-# LANGUAGE TypeFamilies #-}
-{-# LANGUAGE UndecidableInstances #-}
 
 -- |
 -- Typed syntactic sugar on top of @language-glsl@.
 module Shader.Expression.Core
   ( Expr (..),
     toGLSL,
+    ExprF (..),
+    unsafeLift,
   )
 where
 
+import Control.Comonad.Cofree (Cofree ((:<)))
+import Control.Comonad.Trans.Cofree qualified as C
+import Data.Functor.Foldable (cata)
 import Data.Kind (Type)
-import Data.Some (Some)
-import Data.Some qualified as Some
 import GHC.Records (HasField (getField))
-import Graphics.Rendering.OpenGL (GLboolean, GLfloat, GLint)
 import Language.GLSL.Syntax qualified as Syntax
 import Linear (R1, R2, R3, R4)
-import Shader.Expression.Type (Typed)
+import Shader.Expression.Type (Typed (typeOf))
 
 -- | An expression is a GLSL value that has a type. We keep track of the type
 -- of the expression and all its subexpressions in case we want to factor them
 -- out as intermediate bindings.
 type Expr :: Type -> Type
-data Expr x where
-  -- | An integer constant. We can 'Cast' this to @uint@, @float@, or @double@,
-  -- which means we can use 'Shader.Expression.fromInteger' to produce any of
-  -- these types.
-  IntConstant :: Integer -> Expr GLint
-  -- | A floating point constant. We can 'Cast' this to a @double@, so either
-  -- type can be produced using 'Shader.Expression.fromRational'.
-  FloatConstant :: Float -> Expr GLfloat
-  -- | A boolean constant. 'GLboolean' exists in memory as a 'Data.Word.Word8',
-  -- whose @1@ and @0@ values represent 'True' and 'False' respectively.
-  BoolConstant :: Bool -> Expr GLboolean
-  -- | An implicit conversion from one type to another according to the GLSL
-  -- implicit conversion rules.
-  Cast :: (Typed y) => Expr x -> Expr y
-  -- | Use a swizzle mask to select one or more fields from a GLSL vector.
-  FieldSelection :: (Typed y) => String -> Expr x -> Expr y
-  -- | Call a GLSL function with the given arguments.
-  FunctionCall :: (Typed t) => String -> [Some Expr] -> Expr t
-  -- | Add together two GLSL scalars or vectors.
-  Add :: (Typed z) => Expr x -> Expr y -> Expr z
-  -- | Compute the logical conjunction of two GLSL boolean values.
-  And :: Expr GLboolean -> Expr GLboolean -> Expr GLboolean
-  -- | Compute the logical disjunction of two GLSL boolean values.
-  Or :: Expr GLboolean -> Expr GLboolean -> Expr GLboolean
-  -- | Select one of two values based on a GLSL boolean value.
-  Selection :: (Typed x) => Expr GLboolean -> Expr x -> Expr x -> Expr x
+newtype Expr x = Expr {unExpr :: Cofree ExprF Syntax.TypeSpecifier}
+
+-- | The inner data type for 'Expr'. These constructors should map in very
+-- straightforward ways to 'Syntax.Expr'. We express this as a separate functor
+-- so that we can both fold it /and/ reuse it for generating intermediate
+-- binds.
+type ExprF :: Type -> Type
+data ExprF expr
+  = IntConstant Integer
+  | FloatConstant Float
+  | BoolConstant Bool
+  | Cast expr
+  | FieldSelection String expr
+  | FunctionCall String [expr]
+  | Add expr expr
+  | And expr expr
+  | Or expr expr
+  | Selection expr expr expr
+  deriving stock (Functor)
+
+-- | Lift a chunk of AST into an 'Expr' with the given type. This is a very
+-- unsafe function as there's no guarantees about type correctness, and is only
+-- here as a convenience function for internnal definitions.
+unsafeLift :: forall x. (Typed x) => ExprF (Cofree ExprF Syntax.TypeSpecifier) -> Expr x
+unsafeLift xs = Expr (typeOf @x :< xs)
 
 instance (R1 v, Typed e) => HasField "x" (Expr (v e)) (Expr e) where
-  getField = FieldSelection "x"
+  getField (Expr xs) = unsafeLift (FieldSelection "x" xs)
 
 instance (R2 v, Typed e) => HasField "y" (Expr (v e)) (Expr e) where
-  getField = FieldSelection "y"
+  getField (Expr xs) = unsafeLift (FieldSelection "y" xs)
 
 instance (R3 v, Typed e) => HasField "z" (Expr (v e)) (Expr e) where
-  getField = FieldSelection "z"
+  getField (Expr xs) = unsafeLift (FieldSelection "z" xs)
 
 instance (R4 v, Typed e) => HasField "w" (Expr (v e)) (Expr e) where
-  getField = FieldSelection "w"
+  getField (Expr xs) = unsafeLift (FieldSelection "w" xs)
 
 -- | Convert a Haskell expression into a GLSL abstract syntax tree. This
 -- performs no type-checking and is extremely naïve, so optimisations and AST
 -- passes should be done before this call.
 toGLSL :: Expr x -> Syntax.Expr
-toGLSL = \case
-  IntConstant x -> Syntax.IntConstant Syntax.Decimal x
-  FloatConstant x -> Syntax.FloatConstant x
-  BoolConstant x -> Syntax.BoolConstant x
-  Cast x -> toGLSL x
-  Add x y -> Syntax.Add (toGLSL x) (toGLSL y)
-  And x y -> Syntax.And (toGLSL x) (toGLSL y)
-  Or x y -> Syntax.Or (toGLSL x) (toGLSL y)
-  FieldSelection s x -> Syntax.FieldSelection (toGLSL x) s
-  FunctionCall f x -> Syntax.FunctionCall (Syntax.FuncId f) do
-    Syntax.Params (map (Some.foldSome toGLSL) x)
-  Selection p x y -> Syntax.Selection (toGLSL p) (toGLSL x) (toGLSL y)
+toGLSL = cata go . unExpr
+  where
+    go exprF = case C.tailF exprF of
+      IntConstant x -> Syntax.IntConstant Syntax.Decimal x
+      FloatConstant x -> Syntax.FloatConstant x
+      BoolConstant x -> Syntax.BoolConstant x
+      Cast x -> x
+      Add x y -> Syntax.Add x y
+      And x y -> Syntax.And x y
+      Or x y -> Syntax.Or x y
+      FieldSelection s x -> Syntax.FieldSelection x s
+      FunctionCall f xs -> Syntax.FunctionCall (Syntax.FuncId f) (Syntax.Params xs)
+      Selection p x y -> Syntax.Selection p x y
